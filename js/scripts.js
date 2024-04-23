cfc3bb91efb81ea6e049c9809fedc46d9d75d681
Morgan Williams <morganlw500@gmail.com>
2024-04-23 16:54:37 -0600

    nav problem

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,36 @@
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear()
 
+// NAV 
+const hb = document.querySelector('#hamburgerBtn');
+
+hb.addEventListener('click', () => {
+    document.querySelector('#primaryNav').classList.toggle('open')
+})
+
+
+//swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 500,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+
+
+  });
